db9ceeb24ffad37b0e11891b92f2cb147427bea0
Martin Reyes <tincho_427@hotmail.com>
2014-09-06 17:44:11 -0300

    Principio

--- formulario/models.py
+++ formulario/models.py
@@ -3,4 +3,5 @@ from django.db import models
 class Form(models.Model):
     Nombre = models.CharField(max_length=20)
     Apellido = models.CharField(max_length=20)
- 
+    DNI = models.IntegerField(max_length=8)
+
--- formulario/views.py
+++ formulario/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.forms import ModelForm
 
-# Create your views here.
+from formulario import models
+
+
+class FormForm(ModelForm):
+    class Meta:
+        model = models.Form
+
+
+def index(request):
+    #return HttpResponse("Hello, world. You're at the poll index.")
+    form = models.Denuncia(Nombre='Juan')
+    form.save()
+    form = FormForm()
+    context = {'form': form}
+    return render(request, 'formulario/index.html', context)
+
